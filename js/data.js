e8675f529d5f7afdfbd8573e112a83bd2e209ce0
Brian Dowd <dowdbrian@yahoo.com>
2021-03-18 14:27:12 +0100

    added new js file with basic structure

---
+++ js/data.js
@@ -0,0 +1,32 @@
+const gameQuestions = [
+    {
+        question: "Who is the all time top scorer in Premier League history?",
+        answers: {
+            a: "Davy Crocket",
+            b: "Tim Sherwood",
+            c: "Harry Kane",
+            d: "Alan Shearer",
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Which Premier League team finished the season with the fewest points ever?",
+        answers: {
+            a: "Wigan Athletic",
+            b: "Huddersfield",
+            c: "Derby County",
+            d: "Nottingham Forest",
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Who has won the most Premier League titles as manager?",
+        answers: {
+            a: "Arsene Wenger",
+            b: "Sir Alex Ferguson",
+            c: "Jose Mourinho",
+            d: "Pep Guardiola",
+        },
+        correctAnswer: "b"
+    },
+]
---
+++ js/javascript.js
@@ -0,0 +1,34 @@
+const startGameBtn = getElementById('start-game');
+
+startGameBtn.onClick (function () {
+    return gameStart();
+    }
+)
+
+function gameStart () {
+    //onclick event of start button
+    //hides first div display none
+    //displays game div display block
+    //get next question
+};
+
+function getNextQuestion () {
+    //shows the question 
+    //hides previous question
+};
+
+function displayQuestionAnswer () {
+    //show question from data.js
+    //show answers in buttons - loop?
+};
+
+function chooseAnswer () {
+    //Allows to choose answer
+    //checks answers correct/incorrect
+}
+
+function calculateResults () {
+    //displays end game div
+    //shows results
+    //user can return to try again
+}
